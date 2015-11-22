a36776d28ef272a4b1c56f161ab3349006c08266
gulyu <gulyci@hotmail.com>
2015-11-22 16:35:09 +0100

    Prioritás konvertálása + header listviewra

---
+++ SmartNote/SmartNote/PriorityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SmartNote
+{
+    public sealed class PriorityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            //if (parameter == null)
+            //    return value;
+
+            string ret;
+            if ((int)value == 2)
+            {
+                ret = "Magas";
+            }
+            else if ((int)value == 1)
+            {
+                ret = "Közepes";
+            }
+            else
+            {
+                ret = "Alacsony";
+            }
+
+            return ret;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
